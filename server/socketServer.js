3ccf2375caeb8fcd3d646dda73ea1a4fb9191368
Eric Tomasso <etroimcasso@gmail.com>
2020-12-15 14:40:19 -0800

    ???

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,6 +1,14 @@
 "use strict";
 exports.__esModule = true;
 exports.serverFunction = void 0;
+var _REQUEST_getTestInformation = "getTestInformation";
+var _RESPONSE_getTestInformation = "return_getTestInformation";
 var serverFunction = function (client) {
+    var getTestInformation = function (inputString) {
+        console.log("The Socket Server has been sent the string " + inputString);
+        console.log("Return string to client with \"Success\" attached");
+        client.emit();
+    };
+    client.on(_REQUEST_getTestInformation, getTestInformation);
 };
 exports.serverFunction = serverFunction;
--- server/socketServer.ts
+++ server/socketServer.ts
@@ -1,3 +1,21 @@
+const _REQUEST_getTestInformation = "getTestInformation"
+const _RESPONSE_getTestInformation = "return_getTestInformation"
+
 export const serverFunction = (client) => {
-    
+    //#region Get Test Information 
+    const getTestInformation = (inputString: String) => {
+        console.log(`The Socket Server has been sent the string ${inputString}`)
+        console.log(`Return string to client with "Success" attached`)
+
+        client.emit(_RESPONSE_getTestInformation, `SUCCESS: ${inputString}`)
+    }
+
+    client.on(_REQUEST_getTestInformation, getTestInformation)
+    //#endregion
+
+    //#region Send Email
+
+
+    //#endregion
 }
+
