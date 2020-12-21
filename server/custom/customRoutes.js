95124fab32c01d3311e0d8d6803c1670e1e0a77a
Eric Tomasso <eric@tomasso.tech>
2020-12-21 15:47:26 -0800

    Remove some files that shouldn't be synced with git. Add these files to .gitignore

--- server/custom/customRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.customRoutes = void 0;
-var express = require('express');
-var path = require('path');
-var rootPath = require('../src/requiredRoutes').rootPath;
-var customRoutes = function (app) {
-};
-exports.customRoutes = customRoutes;
