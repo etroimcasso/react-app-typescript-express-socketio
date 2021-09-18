54a713c21e3643047e1b696b2873860449894c29
Eric <50342210+etroimcasso@users.noreply.github.com>
2021-09-17 20:35:45 -0700

    Reorganize folder structure a little. Add more scripts to package.json

---
+++ server/routes/customRoutes.ts
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+
+const rootPath = require('../src/requiredRoutes').rootPath
+
+export const customRoutes = (app: any) => {
+
+}
---
+++ server/sockets/socketServer.ts
@@ -0,0 +1,19 @@
+const _REQUEST_getTestInformation = "getTestInformation"
+const _RESPONSE_getTestInformation = "return_getTestInformation"
+
+export const socketServerFunctions = (client) => {
+    //#region Get Test Information 
+    const getTestInformation = (inputString: String) => {
+        console.log(`The Socket Server has been sent the string ${inputString}`)
+        console.log(`Return string to client with "Success" attached`)
+
+        client.emit(_RESPONSE_getTestInformation, `SUCCESS: ${inputString}`)
+    }
+
+    client.on(_REQUEST_getTestInformation, getTestInformation)
+    //#endregion
+    
+    //! Custom socket endpoints go here
+    
+}
+
