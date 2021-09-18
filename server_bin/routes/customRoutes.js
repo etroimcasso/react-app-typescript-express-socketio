012375fcc082020a3e7fe0c951133291862c01cf
Eric <50342210+etroimcasso@users.noreply.github.com>
2021-09-18 12:09:47 -0700

    Remove extra directory from previous commit

--- server_bin/routes/customRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.customRoutes = void 0;
-const express = require('express');
-const path = require('path');
-const rootPath = require('../src/requiredRoutes').rootPath;
-const customRoutes = (app) => {
-};
-exports.customRoutes = customRoutes;
