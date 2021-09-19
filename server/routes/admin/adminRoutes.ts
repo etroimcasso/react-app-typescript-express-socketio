69593c7cc963fd2e1d678765ae15d8270e97d61b
Eric <50342210+etroimcasso@users.noreply.github.com>
2021-09-19 09:14:24 -0700

    Fix erroneous comment

--- server/routes/admin/adminRoutes.ts
+++ server/routes/admin/adminRoutes.ts
@@ -1,10 +1,11 @@
+// App's build location. Use to locate files on the client side
 import { buildPath } from "../../helpers/PathHelper"
 import { WebPathHelper } from "../../helpers/WebPathHelper"
 
 const express = require('express')
 const path = require('path')
 
-// App's build location. Use to locate files on the client side
+
 const routeRoot: String = "/admin"
 const wph: WebPathHelper = new WebPathHelper(routeRoot)
 const wp = (path: String) => wph.path(path)
