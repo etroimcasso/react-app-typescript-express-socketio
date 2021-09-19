9e79ba579eb550963aed5506ae9ea807aee222e7
Eric <50342210+etroimcasso@users.noreply.github.com>
2021-09-19 10:05:00 -0700

    Add comments back to the compiled server files

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -4,9 +4,6 @@ const path = require('path')
 // App's build location. Use to locate files on the client side
 const rootPath = require('../bin/www').rootPath
 
-
-
-
 export const routes = (app: any) => {
     // Add routes from other files here first using 
     require('./admin').routes(app)
