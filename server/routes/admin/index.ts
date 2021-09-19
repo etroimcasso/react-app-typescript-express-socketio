4ed960253b9c5de60bb7696f3a794d626f5e83b1
Eric <50342210+etroimcasso@users.noreply.github.com>
2021-09-19 09:25:20 -0700

    Fix issue with typescript express types

--- server/routes/admin/index.ts
+++ server/routes/admin/index.ts
@@ -5,19 +5,18 @@ import { WebPathHelper } from "../../helpers/WebPathHelper"
 const express = require('express')
 const path = require('path')
 
-
 const routeRoot: String = "/admin"
 const wph: WebPathHelper = new WebPathHelper(routeRoot)
 const wp = (path: String) => wph.path(path)
 
 export const routes = (app: any) => {
     // GET: Login
-    app.get(wp("login"), (req: Express.Request, res: Express.Response) => {
-
+    app.get(wp("login"), (req, res) => {
+        
     })
 
     // POST: Login
-    app.post(wp("login"), (req: Express.Request, res: Express.Response) => {
+    app.post(wp("login"), (req, res) => {
 
     })
 }
--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -4,7 +4,13 @@ const path = require('path')
 // App's build location. Use to locate files on the client side
 const rootPath = require('../bin/www').rootPath
 
+
+
+
 export const routes = (app: any) => {
+    // Add routes from other files here first using 
+    require('./admin').routes(app)
+    
     app.get('/test', (req, res) => {
         res.send(`App build directory is: ${rootPath}`)
     })
