0d79eeccce6be5b76796dc5f4964fe7c09a88384
Eric Tomasso <eric@tomasso.tech>
2023-03-14 14:53:31 -0700

    Add checkForRequiredBodyParemeters to server/helpers/RouteHelper. This function is the same as checkForRequiredParemeters, except only the parameter names are required for testing now instead of the name and the value itself extracted from req.

--- server/helpers/RouteHelper.ts
+++ server/helpers/RouteHelper.ts
@@ -19,18 +19,18 @@ export const checkForRequiredParameters = async (res, missingParametersCheck: Pa
     return new Promise<boolean>((resolve, reject) => resolve(true))
 }
 
-// export const checkForRequiredParameters2 = async (res, missingParametersCheck: ParameterCheckTuple[]): Promise<boolean> => {
-//     const missingParameters: ParameterCheckTuple[] = missingParametersCheck.filter(item => item[1])
-//     if (missingParameters.length > 0) {
-//         const errorString = missingParameters.reduce((acc, cv, index, array) => 
-//             `${acc} ${cv[0]}${index == array.length - 1 ? "." : ","}`
-//             // , `Missing propert${missingParameters.length > 1 ? "ies" : "y"}:`)
-//             , `Missing ${makePlural("property", missingParameters.length, undefined, "properties")}`)
+export const checkForRequiredBodyParameters = async (req, res, missingParametersCheck: string[]): Promise<boolean> => {
+    const missingParameters: string[] = missingParametersCheck.filter(item => typeof(req.body[item]) !== 'undefined' || req.body[item] !== '')
+    
+    if (missingParameters.length > 0) {
+        const errorString = missingParameters.reduce((acc, cv, index, array) => 
+            `${acc} ${cv}${index == array.length - 1 ? "" : ","}`
+            , `Missing propert${missingParameters.length > 1 ? "ies" : "y"}:`)
 
-//         respondWithError(res, StatusCodes._400, {
-//             errorCode:  "MISSING_PROPERTIES", 
-//             errorMessage: errorString
-//         })
-//     }
-//     return new Promise<boolean>((resolve, reject) => resolve(true))
-// }
+        respondWithError(res, StatusCodes._400, {
+            errorCode:  "MISSING_PROPERTIES", 
+            errorMessage: errorString
+        })
+    }
+    return new Promise<boolean>((resolve, reject) => resolve(true))
+}
