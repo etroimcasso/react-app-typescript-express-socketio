e2ddd89bda75bc44ed5dff5ad6ef7f8f5e4ba56e
Eric Tomasso <eric@tomasso.tech>
2023-07-13 00:56:36 -0700

    Move bootCheck.ts to server/util. Add RandomStringGenerator

---
+++ server/helpers/RandomStringGenerator.ts
@@ -0,0 +1,44 @@
+import { randomRange } from "./RandomNumberGenerator";
+
+export const generateRandomString = (length: number) => {
+    /*
+    (Total number of characters available minus _ and - for first character)
+    - (total number of characters available)^(string length - 1)
+    */
+    // const totalPossibleCharacters= 62
+    // const totalPossible = Math.pow(totalPossibleCharacters, length)
+
+    //Generates the actual string
+    /*
+        CHARACTER CODES:
+        65 - 90 : Uppercase
+        97 - 122: lowercase
+    */
+    return Array.from(Array(length).keys()).reduce((fullString: string): string => {
+        const nextCharacter: string | number = (() => {
+            switch (randomRange(0,8)) {
+                case 0:
+                case 1:
+                case 2:
+                    //uppercase
+                    return String.fromCharCode(randomRange(65,90))
+                case 3:
+                case 4:
+                case 5:
+                    //lowercase
+                    return String.fromCharCode(randomRange(97,122))
+                case 6:
+                case 7:
+                case 8:
+                    //number
+                    return randomRange(0,9)
+                default:
+                    return 0
+            }
+        })()
+        
+        return `${fullString}${nextCharacter}`
+
+    }, "")
+
+}
